fdc3737bc8cedc0a3b169b58fbf62666f9a59a40
ouaqa <aurelien.benefice@teads.tv>
2019-12-02 17:58:39 +0100

    step-3 I'm feeling thristy - starter

--- react-workshop/v3-beertender-im-feeling-thirsty/src/randomBeerActions.js
+++ react-workshop/v3-beertender-im-feeling-thirsty/src/randomBeerActions.js
@@ -12,18 +12,24 @@ export const handleRandomBeerResponse = randomBeer => ({
 
 export const getRandomBeer = () => async dispatch => {
   dispatch(requestRandomBeer())
-  const result = await (await window.fetch(
-    `https://api.punkapi.com/v2/beers/random`
-  )).json();
+  const result = await fetchRandomBeer()
   dispatch(
     handleRandomBeerResponse(
-      ...result.map(({ id, name, image_url, food_pairing, description }) => ({
-        id,
-        name,
-        url: image_url,
-        food_pairing,
-        description
-      }))
+      result
     )
-  );
-};
+  )
+}
+
+const fetchRandomBeer = async () => {
+  const payload = await (await window.fetch(
+    `https://api.punkapi.com/v2/beers/random`
+  )).json()
+  const result = payload.map(({ id, name, image_url, food_pairing, description }) => ({
+    id,
+    name,
+    url: image_url,
+    food_pairing,
+    description
+  }))[0]
+  return result
+}
